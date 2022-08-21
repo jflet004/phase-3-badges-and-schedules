5a3a8b5e1bbf03b5f5505498480ea727e909c5fc
Jose Fletes <joseangelfletes@gmail.com>
2022-08-21 11:08:33 -0700

    commit tree 89c68201cdef8134f9b969ce277b00ed1716e08a

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
+require "pry"
+
 # Write your code here.
+# array_of_names = ["Jose", "Batboy", "Cassandra"]
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array_of_names)
+  array_of_names.collect{|names| badge_maker(names)}
+end
+
+def assign_rooms(array_of_names)
+  array_of_names.map.with_index do |name, index|
+    if index < 7
+      "Hello, #{name}! You'll be assigned to room #{index+1}!"
+    end
+  end
+end
+
+def printer(array_of_names)
+  batch_badge_creator(array_of_names).each{|name| puts name}
+  assign_rooms(array_of_names).each{|room_assignment| puts room_assignment}
+end
+
+# binding.pry
